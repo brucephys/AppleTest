9e6d98f8acc85e6d5a284e0576d6455639dd21b2
Lung Li <bruce.phy@gmail.com>
2020-10-22 17:27:55 +0800

    Updates

---
+++ precache-manifest.adea379ffed3249a4db11b5617615bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37333052724ba911cd02ecbaa62d5a8",
+    "url": "/appletest/index.html"
+  },
+  {
+    "revision": "5cbaf629e56b364e7dd7",
+    "url": "/appletest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6848a67324d2ef9e384",
+    "url": "/appletest/static/js/2.c31f35db.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/appletest/static/js/2.c31f35db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbaf629e56b364e7dd7",
+    "url": "/appletest/static/js/main.51a255e5.chunk.js"
+  },
+  {
+    "revision": "bbcf99897c69aa6e7fa7",
+    "url": "/appletest/static/js/runtime-main.6d2b67cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/appletest/static/media/logo.5d5d9eef.svg"
+  }
+]);
